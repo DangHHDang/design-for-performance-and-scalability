b1cc903a4a098a79eaf140d0bf8f61737f508198
dangdhh <dangdang0511gmail.com>
2022-10-03 22:39:44 +0700

    project2

--- Exercise_1/main.tf
+++ Exercise_1/main.tf
@@ -1,7 +1,37 @@
-# TODO: Designate a cloud provider, region, and credentials
+terraform {
+    required_providers {
+        aws = {
+            source  = "hashicorp/aws"
+            version = "~> 4.16"
+        }
+    }
 
+    required_version = ">= 1.2.0"
+}
+
+# TODO: Designate a cloud provider, region, and credentials
+provider "aws" {
+  profile = "dangdhh"
+  region  = "us-west-2"
+}
 
 # TODO: provision 4 AWS t2.micro EC2 instances named Udacity T2
-
+resource "aws_instance" "Udacity_T2" {
+  count         = "4"
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "Udacity T2"
+  }
+}
 
 # TODO: provision 2 m4.large EC2 instances named Udacity M4
+
+resource "aws_instance" "Udacity_M4" {
+  count         = "2"
+  ami           = "ami-830c94e3"
+  instance_type = "m4.large"
+  tags = {
+    Name = "Udacity M4"
+  }
+}
--- Exercise_2/variables.tf
+++ Exercise_2/variables.tf
@@ -1,1 +1,8 @@
 # TODO: Define the variable for aws_region
+variable "lambda_output_path" {
+  default = "output.zip"
+}
+
+variable "lambda_name" {
+  default = "greet_lambda"
+}
